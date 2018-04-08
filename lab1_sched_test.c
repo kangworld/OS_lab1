ea15c135a02aec6e36bbd95b00a2c51d5d520058
kangmingu <beat6744@nate.com>
2018-04-08 23:23:09 +0900

    MLFQ/FIFO

--- lab1_sched_test.c
+++ lab1_sched_test.c
@@ -25,7 +25,7 @@
 #include <assert.h>
 #include <pthread.h>
 #include <asm/unistd.h>
-
+#include <stdbool.h>
 #include "lab1_sched_types.h"
 
 /*
@@ -33,7 +33,53 @@
  *
  */
 
-int main(int argc, char *argv[]){
-	//프로세스 생성 
-}
+enum scheduler { FIFO = 1, SJF, RR, MLFQ, LOTTERY, QUIT };
 
+int main(int argc, char *argv[]){
+	bool **workLoad = NULL;
+	bool flag = true;
+	int totalServiceTime = 0;
+	int selectMenu;
+	Process processSet[PROCESS_COUNT] = {
+		//도착시작, 총 수행시간, 현재 수행시간, 축적시간, 프로세스 아이디
+		{ 0, 3, 0, 0 ,0 },
+		{ 2, 6, 0, 0, 1 },
+		{ 4, 4, 0, 0, 2 },
+		{ 6, 5, 0, 0, 3 },
+		{ 8, 2, 0, 0, 4 },
+	};
+
+	for(int i = 0; i < 5; i++){
+		totalServiceTime += processSet[i].serviceTime;	
+	}
+
+	workLoad = createWorkLoad(totalServiceTime, workLoad);
+
+	while(flag){
+		showMenu();
+		printf("Select menu : ");
+		scanf("%d", &selectMenu);
+		initComponent(processSet, workLoad, totalServiceTime);
+		switch(selectMenu){
+			case FIFO :
+				firstInFirstOut(processSet, workLoad, totalServiceTime);
+				showWorkLoad(totalServiceTime, workLoad);
+				break;
+			case SJF :
+				break;
+			case RR :
+				break;
+			case MLFQ :
+				multilevelFeedbackQueue(processSet, workLoad, totalServiceTime, 1);
+				showWorkLoad(totalServiceTime, workLoad);
+			case LOTTERY :
+				break;
+			case QUIT :
+				flag = false;
+				break;
+			default :
+				printf("Wrong number\n");
+				break;
+		}
+	}
+}
