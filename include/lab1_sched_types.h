518a2d350e43da55277776574a83fdbfebd889f7
kangmingu <beat6744@nate.com>
2018-04-08 23:18:52 +0900

    FIFO/MLFQ

--- include/lab1_sched_types.h
+++ include/lab1_sched_types.h
@@ -18,10 +18,14 @@
  * You need to Declare functions in  here
  */
 
+#define PROCESS_COUNT 5
+
 typedef struct process{
-	char name[10]; // 프로세스 이름
 	int arriveTime; // 프로세스 도착 시간
-	int serviceTime; // 프로세스 수행 시간
+	int serviceTime; // 프로세스 요구 수행 시간
+	int currentServiceTime; // 실제 프로세스 수행 시간
+	int accumulatedTime; // MLFQ에 사용될 누적시간
+	int processId;
 }Process;
 
 typedef struct node{
@@ -33,6 +37,7 @@ typedef struct queue{
 	Node *front; // 맨 앞(프로세스 꺼낼 위치)
 	Node *rear; // 맨 뒤(보관할 위치)
 	int count; // 보관 개수
+	int priority;
 }Queue;
 
 void initQueue(Queue *queue); // 큐 초기화
@@ -41,5 +46,10 @@ void enQueueFront(Queue *queue, Process *process); // 큐 front에 프로세스 
 void enQueueRear(Queue *queue, Process *process); // 큐 rear에 프러세스 삽입
 Process * deQueue(Queue *queue); // 큐에서 프로세스 삭제
 
-void firstInFirstOut(void);
+void firstInFirstOut(Process *process, bool **workLoad, int totalServiceTime); // FIFO 스케쥴러 함수
+void multilevelFeedbackQueue(Process *process, bool **workLoad, int totalServiceTime, int timeSlice);
+void showWorkLoad(int totalRunningTime, bool **workLoad); // workLoad 출력함수
+bool ** createWorkLoad(int totalRunningTime, bool **workLoad); 
+void showMenu(void);
+void initComponent(Process *processSet, bool **workLoad, int totalServiceTime);
 #endif /* LAB1_HEADER_H*/
--- lab1_sched.c
+++ lab1_sched.c
@@ -44,6 +44,18 @@ bool isEmpty(Queue *queue){
 	}
 }
 
+/*
+   MLFQ에서 모든 큐가 공백인지 검사하는 함수
+ */
+bool isAllEmpty(Queue *multiQueue, int queueLevel){
+	for(int i = 0; i < queueLevel; i++){
+		if(isEmpty(&multiQueue[i]) == false){
+			return false;
+		}
+	}
+	return true;
+}
+
 void enQueueRear(Queue *queue, Process *process){
 	Node *newNode = (Node *)malloc(sizeof(Node));
 	newNode->process = process;
@@ -87,3 +99,137 @@ Process * deQueue(Queue *queue){
 		return delProc;
 	}
 }
+
+void firstInFirstOut(Process *processSet, bool **workLoad, int totalRunningTime){
+	Queue readyQue;
+	int timer = 0;
+	int processIndex = 0;
+	Process *runningProcess = NULL;
+	initQue(&readyQue);
+	//workLoad = initWorkLoad(processCount, totalRunningTime, workLoad);
+
+	for(; timer < totalRunningTime; timer++){
+		while(processSet[processIndex].arriveTime == timer){
+			enQueueRear(&readyQue, &processSet[processIndex]);
+			processIndex++;
+		}
+		if(runningProcess == NULL){
+			runningProcess = deQueue(&readyQue);
+		}
+		runningProcess -> currentServiceTime++;
+		workLoad[runningProcess -> processId][timer] = true;
+		if(runningProcess -> serviceTime == runningProcess -> currentServiceTime){
+			runningProcess = NULL;
+		}
+	}
+}
+
+void multilevelFeedbackQueue(Process *processSet, bool **workLoad, int totalRunningTime, int timeSlice){
+	Queue multiQueue[4];
+	int timer = 0;
+	int processIndex = 0;
+	int queueLevel = 4;
+	int processPriority = 0;
+
+	Process *runningProcess = NULL;
+
+	for(int i = 0; i < queueLevel; i++){
+		initQue(&multiQueue[i]);
+		multiQueue[i].priority = i;
+	}
+//	workLoad = initWorkLoad(processCount, totalRunningTime, workLoad);
+
+	for(; timer < totalRunningTime;){
+		
+		while(processSet[processIndex].arriveTime == timer){
+			enQueueFront(&multiQueue[0], &processSet[processIndex]);
+			processIndex++;
+		}
+		if(runningProcess == NULL){ //작업중인 큐가 없으면 큐에 새로운 잡을 넣어줌
+			for(int i = 0; i < queueLevel; i++){
+				runningProcess = deQueue(&multiQueue[i]); // 우선순위가 높은 큐에 있는 잡부터 수행프로세스에 삽입
+				processPriority = i;
+				if(runningProcess != NULL){ // 러닝프로세스에 들오면 더이상 큐에서 프로세스를 빼지 않음
+					break;
+				}
+			}
+		}
+		runningProcess -> currentServiceTime++;
+		runningProcess -> accumulatedTime++;
+		workLoad[runningProcess -> processId][timer] = true;
+		timer++; // 타이머 증가
+		
+		if(runningProcess -> currentServiceTime == runningProcess -> serviceTime){
+			runningProcess = NULL; //프로세스 종료
+		}else if(runningProcess -> accumulatedTime == timeSlice){ //아직 수행할 시간이 더 남았음
+			if(processPriority == (queueLevel - 1)){ 
+				processPriority--;
+			}
+
+			if(processSet[processIndex].arriveTime == timer + 1 || !isAllEmpty(multiQueue, queueLevel)){
+				if(processPriority != (queueLevel - 1)){
+					processPriority++;
+				}
+				enQueueRear(&multiQueue[processPriority], runningProcess);
+			}else{
+				enQueueRear(&multiQueue[processPriority], runningProcess);
+			}
+			runningProcess -> accumulatedTime = 0;
+			runningProcess = NULL;
+		}
+	}
+}
+
+bool ** createWorkLoad(int totalRunningTime, bool **workLoad){
+	workLoad = (bool**) malloc (sizeof(bool*) * PROCESS_COUNT);
+    for(int i = 0; i < PROCESS_COUNT; i++){
+         workLoad[i] = (bool*) malloc (sizeof(bool) * totalRunningTime);
+         for(int j = 0; j < totalRunningTime; j++){
+			 workLoad[i][j] = false;
+		 }
+    }
+	return workLoad;
+}
+
+void showWorkLoad(int totalRunningTime, bool **workLoad){
+	char processName[PROCESS_COUNT] = {'A', 'B', 'C', 'D', 'E'};
+
+	for(int i = 0; i < PROCESS_COUNT; i++){
+		printf("%c :", processName[i]);
+		for(int j =0; j < totalRunningTime; j++){
+			if(workLoad[i][j] == true){
+				printf(" ■ ");
+			}else{
+				printf(" □ ");
+			}
+		}
+		printf("\n");
+	}
+}
+
+void showMenu(void){
+	printf("-------------------------------------\n");
+	printf("1. First come first serve\n");
+	printf("2. Shortest job first\n");
+	printf("3. Round robin\n");
+	printf("4. Multilevel feedback queue\n");
+	printf("5. Lottery\n");
+	printf("6. QUIT\n");
+	printf("-------------------------------------\n");
+}
+
+void initComponent(Process *processSet, bool **workLoad, int totalServiceTime){
+	//Process 설정 초기화
+	for(int i = 0; i < PROCESS_COUNT; i++){
+		processSet[i].currentServiceTime = 0;
+		processSet[i].accumulatedTime = 0;
+	}
+
+	//workLoad 초기화
+	for(int i = 0; i < PROCESS_COUNT; i++){
+		for(int j = 0; j < totalServiceTime; j++){
+				workLoad[i][j] = false;
+		}
+	}
+}
+
